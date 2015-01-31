0da99c7e9f8cc38224fe54df52f6f4fd27d9df98
Bei Chen Liu <bei.liu@mail.mcgill.ca>
2015-01-31 03:01:46 -0500

    add 12 /24 hour format, update column image, etc

--- src/jake.c
+++ src/jake.c
@@ -1,7 +1,7 @@
 #include <pebble.h>
 
 #define ANIMATION_DURATION 400
-#define AMERICAN_FORMAT 1
+#define AMERICAN_FORMAT
 
 static Window *window;
 static BitmapLayer *background_layer;
@@ -18,6 +18,8 @@ static Layer *date_layer;
 static BitmapLayer *date_num_layer[7];
 static GBitmap *date_num_img[7];
 
+static bool initialized = false;
+
 static int time_cache[4]={-1,-1,-1,-1}; //hour 10s, hour 1s,minit 10s,minut 1s
 static int date_cache[7]={-1,-1,-1,-1,-1,-1,-1};//wday, month 10s, month 1s, date 10s, date 1s, year 10s, year 1s
 static int time_now[4];
@@ -27,7 +29,6 @@ static GRect time_from_rect;
 static GRect time_to_rect;
 static GRect date_from_rect;
 static GRect date_to_rect;
-
 static PropertyAnimation *time_animation_beg;
 static PropertyAnimation *time_animation_end;
 static PropertyAnimation *date_animation_beg;
@@ -38,9 +39,9 @@ static const char time_num_position[5][4] = {
     { 29,0,26,34}, //hour 1s
     { 73,0,26,34}, //minuit 10s
     {102,0,26,34},  //minuit 1s
-    { 58,3,12,31} // :
+    { 58,0,12,34} // :
 };
-#if (AMERICAN_FORMAT)
+#ifdef AMERICAN_FORMAT
 static const char date_num_position[7][4] ={
     {  0,0,25,15}, //day
     { 32,0,12,15}, //month 10s
@@ -124,7 +125,6 @@ void time_animation_stopped(Animation *animation, void *data) {
     }
     
     layer_set_hidden(bitmap_layer_get_layer(battery_low_layer),battery_state_service_peek().charge_percent > 25); //charge indication
-    //APP_LOG(APP_LOG_LEVEL_DEBUG,"Battery Level: %d",battery_state_service_peek().charge_percent);
     layer_mark_dirty(time_layer);
     animation_schedule((Animation*) time_animation_end);
 }
@@ -152,59 +152,92 @@ void date_animation_stopped(Animation *animation, void *data) {
             date_cache[i]=date_now[i];
             gbitmap_destroy(date_num_img[i]);
             date_num_img[i]= gbitmap_create_with_resource(date_num_id[date_now[i]]);
-            bitmap_layer_set_bitmap(date_num_layer[i],date_num_img[i]);
-            
-        }
-        
+            bitmap_layer_set_bitmap(date_num_layer[i],date_num_img[i]);   
+        }   
     }
     
     layer_mark_dirty(date_layer);
     animation_schedule((Animation*) date_animation_end);
 }
 
-void handle_minuit_tick(struct tm *tick_time, TimeUnits units_changed){
-    char temp = tick_time->tm_hour;
-    if(!clock_is_24h_style()){
-        temp=temp%12;
+//Parsing time and display time
+void display_time(struct tm *tick_time){
+  //hour
+  int hour = tick_time->tm_hour;
+  //12h and 24 hour support
+  if(!clock_is_24h_style()){
+    hour = hour % 12;
+      if(hour == 0){
+        hour = 12;
+      }
     }
-    time_now[0] = temp/10;
-    time_now[1] = temp%10;
-    temp = tick_time->tm_min;
-    time_now[2] = temp/10;
-    time_now[3] = temp%10;
-    date_now[0] = tick_time->tm_wday;
-    temp = tick_time->tm_mon;
-    date_now[1] = temp/10;
-    date_now[2] = temp%10+1;
-    temp = tick_time->tm_mday;
-    date_now[3] = temp/10;
-    date_now[4] = temp%10;
-    temp = (tick_time->tm_year)%100;
-    date_now[5] = temp/10;
-    date_now[6] = temp%10;
-    
-    property_animation_destroy(time_animation_beg);
-    time_animation_beg= property_animation_create_layer_frame(time_layer,&time_to_rect,&time_from_rect);
-    animation_set_curve(&time_animation_beg->animation, AnimationCurveLinear);
-    animation_set_duration( &time_animation_beg->animation, ANIMATION_DURATION);
-    animation_set_handlers((Animation*) time_animation_beg, (AnimationHandlers) {
-        .started = NULL,
-        .stopped = (AnimationStoppedHandler) time_animation_stopped
-    }, 0);
-    animation_schedule((Animation*) time_animation_beg);
-    
-    if(date_now[4]!=date_cache[4]){
-        property_animation_destroy(date_animation_beg);
-        date_animation_beg= property_animation_create_layer_frame(date_layer,&date_to_rect,&date_from_rect);
-        animation_set_curve(&date_animation_beg->animation, AnimationCurveLinear);
-        animation_set_duration( &date_animation_beg->animation, ANIMATION_DURATION);
-        animation_set_handlers((Animation*) date_animation_beg, (AnimationHandlers) {
-            .started = NULL,
-            .stopped = (AnimationStoppedHandler) date_animation_stopped
-        }, 0);
-        animation_schedule((Animation*) date_animation_beg);
-    }
+  time_now[0] = hour/10;
+  time_now[1] = hour%10;
+  
+  //minuit
+  int minuit = tick_time->tm_min;
+  time_now[2] = minuit/10;
+  time_now[3] = minuit%10;
+  
+  //start animations
+  property_animation_destroy(time_animation_beg);
+  time_animation_beg= property_animation_create_layer_frame(time_layer,&time_to_rect,&time_from_rect);
+  animation_set_curve(&time_animation_beg->animation, AnimationCurveLinear);
+  animation_set_duration( &time_animation_beg->animation, ANIMATION_DURATION);
+  animation_set_handlers((Animation*) time_animation_beg, (AnimationHandlers){
+    .started = NULL,
+    .stopped = (AnimationStoppedHandler) time_animation_stopped
+  }, 0);
+  animation_schedule((Animation*) time_animation_beg);
 }
+
+//Parsing data and display date
+void display_date(struct tm *tick_time){
+  //Day of the week
+  date_now[0] = tick_time->tm_wday;
+  
+  //Month
+  int month = tick_time->tm_mon + 1;
+  date_now[1] = month / 10;
+  date_now[2] = month % 10;
+  //Date 
+  
+  int date = tick_time->tm_mday;
+  date_now[3] = date / 10;
+  date_now[4] = date % 10;
+  
+  //Year
+  int year = (tick_time->tm_year) % 100;
+  date_now[5] = year / 10;
+  date_now[6] = year % 10;
+  
+  //start animation
+  if(date_now[4] != date_cache[4]){
+    property_animation_destroy(date_animation_beg);
+    date_animation_beg= property_animation_create_layer_frame(date_layer,&date_to_rect,&date_from_rect);
+    animation_set_curve(&date_animation_beg->animation, AnimationCurveLinear);
+    animation_set_duration( &date_animation_beg->animation, ANIMATION_DURATION);
+    animation_set_handlers((Animation*) date_animation_beg, (AnimationHandlers){
+      .started = NULL,
+      .stopped = (AnimationStoppedHandler) date_animation_stopped
+    }, 0);
+    animation_schedule((Animation*) date_animation_beg); 
+  }  
+}
+  
+
+void handle_minuit_tick(struct tm *tick_time, TimeUnits units_changed){
+  
+  display_time(tick_time);
+    
+  if (((units_changed & DAY_UNIT) == DAY_UNIT) || (!initialized)) {  
+    initialized = true;
+    display_date(tick_time);
+  }
+  
+}
+
+
 
 
 static void window_load(Window *window) {
@@ -229,25 +262,28 @@ static void window_load(Window *window) {
         layer_add_child(time_layer,bitmap_layer_get_layer(time_num_layer[i]));
     }
 //init date layer
-    date_layer = layer_create(date_to_rect);
-    layer_set_clips(date_layer,1);
-    layer_add_child(window_layer,date_layer);
-    for(int i = 0; i<7 ;i++){
-        date_num_layer[i] = bitmap_layer_create(GRect(date_num_position[i][0],date_num_position[i][1],date_num_position[i][2],date_num_position[i][3]));
-        layer_add_child(date_layer,bitmap_layer_get_layer(date_num_layer[i]));
-    }
-//init battery low layer
-    battery_low_layer = bitmap_layer_create(GRect(66,3,12,31));
-    battery_low_img = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BAT_LOW);
-    bitmap_layer_set_bitmap(battery_low_layer,battery_low_img);
-    layer_set_hidden(bitmap_layer_get_layer(battery_low_layer),1);
-    layer_add_child(window_layer,bitmap_layer_get_layer(battery_low_layer));
-//init time tick callback
-    tick_timer_service_subscribe(MINUTE_UNIT,handle_minuit_tick);
+  date_layer = layer_create(date_to_rect);
+  layer_set_clips(date_layer,1);
+  layer_add_child(window_layer,date_layer);
+  for(int i = 0; i<7 ;i++){
+    date_num_layer[i] = bitmap_layer_create(GRect(date_num_position[i][0],date_num_position[i][1],date_num_position[i][2],date_num_position[i][3]));
+    layer_add_child(date_layer,bitmap_layer_get_layer(date_num_layer[i]));
+  }
+  
+  //init battery low layer
+  battery_low_layer = bitmap_layer_create(GRect(66,3,12,31));
+  battery_low_img = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BAT_LOW);
+  bitmap_layer_set_bitmap(battery_low_layer,battery_low_img);
+  layer_set_hidden(bitmap_layer_get_layer(battery_low_layer),1);
+  layer_add_child(window_layer,bitmap_layer_get_layer(battery_low_layer));
+  
+  //init time tick callback
+  tick_timer_service_subscribe(MINUTE_UNIT,handle_minuit_tick);
     
 }
 
 static void window_unload(Window *window) {
+    initialized = false;
     tick_timer_service_unsubscribe();
     property_animation_destroy(time_animation_beg);
     property_animation_destroy(time_animation_end);
